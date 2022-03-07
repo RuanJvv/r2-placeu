1872f2f3bc6a3dd4274e96285e0dc93c3d18504c
Ruan Jansen van Vuuren <ruan@inventixx.com>
2022-03-07 12:02:20 +0200

    Project initialization

---
+++ src/main/java/com/r2placeu/main/R2PlaceuApplication.java
@@ -0,0 +1,13 @@
+package com.r2placeu.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2PlaceuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2PlaceuApplication.class, args);
+	}
+
+}
